48df7c905bec3ebfc6f34bcff943471635742d5f
Robel-robel <robelberhanu89@gmail.com>
2022-11-02 14:10:25 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyexpat import model
 import re
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel # used to define schema.
 from random import randrange
@@ -34,7 +34,7 @@ async def get_posts():
 
 # send posts to server
 
-@app.post("/createposts")
+@app.post("/createposts", status_code = status.HTTP_201_CREATED)
 async def create_posts(post: Post):
      post_dict = post.dict()
      post_dict['id'] = randrange(0,1000000)
@@ -44,5 +44,8 @@ async def create_posts(post: Post):
 # get single post
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)  
+    post = find_post(id) 
+    if not post:
+       raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, 
+                           detail= f'post with id : {id} not found')
     return{"post_detail": post}
