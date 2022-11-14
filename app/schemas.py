c1ea37448f3b57fc40283a41d70cdf971efd2dbe
Robel-robel <robelberhanu89@gmail.com>
2022-11-14 09:22:53 +0200

    created pydentic model for users schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel # used to define schema.
+from pydantic import BaseModel, EmailStr # used to define schema.
 from datetime import datetime
 
 
@@ -14,10 +14,24 @@ class PostCreate(PostBase):
 
 # Base Model For Sending data to User(Response Schema For Posts)
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
+class Post(PostBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserCreate(BaseModel):
+    id:int
+    email: EmailStr
+    password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
 
     class Config:
         orm_mode = True
