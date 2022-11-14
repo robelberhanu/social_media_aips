bfb873695b5f9113aee1c04f24f20b1e56a6df48
Robel-robel <robelberhanu89@gmail.com>
2022-11-14 09:19:42 +0200

    created path operation for user signup

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from pyexpat import model
 import re
-from typing import Optional
+from typing import Optional, List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from random import randrange
@@ -15,58 +15,27 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 
-# Connecting to a database 
-# while True:
-#     try:
-#         conn = psycopg2.connect(host='localhost', database = 'socialmedia', user='postgres', password='roblion23', cursor_factory=RealDictCursor)
-#         cursor = conn.cursor()
-#         print("database connection was successful")
-#         break
-#     except Exception as error:
-#         print("connection failed")
-#         print("error:", error)
-#         time.sleep(2)
 
-
-# my_posts = [{"title": "title of post 1", "content":"content of post one", "id":1}, {"title": "favorite foods", "content":"I like pizza", "id":2}]
-
-# Helper functions
-# def find_post(id):
-#     for p in my_posts:
-#         if p['id'] == id:
-#             return p
-
-# def find_post_index(id):
-#     for i, p in enumerate(my_posts):
-#         if p['id'] == id: 
-#             return i
 
 # root path
 @app.get('/')
 async def root():
     return{"message":"Hello"}
 
+
+####################################################### POSTS ########################################################
+
 # Get posts path
-@app.get("/getposts")
-def test_posts(db: Session = Depends(get_db)):
+@app.get("/posts", response_model=List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
     posts = db.query(models.Post).all()
     return posts
 
 
-#get posts path
-# @app.get('/getposts')
-# async def get_posts():
-#     cursor.execute("""SELECT * FROM posts""")
-#     posts = cursor.fetchall()
-#     return{"data":posts}
 
 # send posts to server
-@app.post("/createposts", status_code = status.HTTP_201_CREATED, response_model=schemas.Post)
+@app.post("/posts", status_code = status.HTTP_201_CREATED, response_model=schemas.Post)
 async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
-    #  cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
-    #  (post.title, post.content, post.published))
-    #  new_post = cursor.fetchone()
-    #  conn.commit()
     new_post =  models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -75,11 +44,9 @@ async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return new_post
 
 # get single post
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model= schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""SELECT * from posts WHERE id = %s""", (str(id),))
-    # post = cursor.fetchone()
-    
+  
     post = db.query(models.Post).filter(models.Post.id == id).first() # will fine the first post with specified ID only. 
     if not post:
        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, 
@@ -87,12 +54,9 @@ def get_post(id: int, db: Session = Depends(get_db)):
     return post
 
 # Delete post Endpoint
-@app.delete('/deletepost/{id}', status_code = status.HTTP_204_NO_CONTENT)
+@app.delete('/posts/{id}', status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
-    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    # deleted_post = cursor.fetchone()
-    # conn.commit()
-
+   
     post = db.query(models.Post).filter(models.Post.id == id)
     if post == None:
         raise HTTPException(status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} does not exist')
@@ -102,11 +66,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 # Update Posts
-@app.put("/updatepost/{id}")
+@app.put("/posts/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
-    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s  WHERE id = %s RETURNING *""", (post.title, post.content, post.published, (str(id))))
-    # updated_post = cursor.fetchone()
-    # conn.commit()
     post_query = db.query(models.Post).filter(models.Post.id == id)
     post = post_query.first()
 
@@ -117,5 +78,20 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     db.commit()
     return post_query.first()
 
+
+#########################################################End Posts #########################################################
+
+
+######################################################## Account Services ######################################################
+
+# path operation to create users.
+@app.post("/users", status_code = status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user =  models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
 
 
