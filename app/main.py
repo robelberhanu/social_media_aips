85b2299ec82c159287c338dcca059870ab9c3464
Robel-robel <robelberhanu89@gmail.com>
2022-12-03 16:54:51 +0200

    updated main.py

--- app/main.py
+++ app/main.py
@@ -94,14 +94,6 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
     return new_user
 
-
-@app.post("/users", status_code = status.HTTP_201_CREATED, response_model=schemas.UserOut)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    new_user =  models.User(**user.dict())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
+ 
 
 
