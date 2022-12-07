0bc8cb7db0276965e388e834ff3e585b23f0ec4e
Robel-robel <robelberhanu89@gmail.com>
2022-12-07 18:05:17 +0200

    added auth router for user login.

---
+++ app/routers/auth.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, Response, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from ..import database, schemas, models
+
+router = APIRouter(tags='Authentication')
+
+@router.post('/login')
+def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+
+    if not user:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
+
+    
+
