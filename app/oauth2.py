2ebf164c979e5b6fe33fcf9c50ce87246d02af61
Robel-robel <robelberhanu89@gmail.com>
2022-12-22 12:08:11 +0200

    created oauth2.py to add jwt access_token logic and modified auth.py to use use the oauth2 class properties for token generation

---
+++ app/oauth2.py
@@ -0,0 +1,18 @@
+from jose import JWTError, JWTError, jwt
+from datetime import datetime, timedelta
+
+# SECRET_KEY
+# Algorithm
+# Expiration time
+
+
+SECRET_KEY = "hello"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRATION_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRATION_MINUTES)
+    to_encode.update({"exp" : expire})
+
+    jwt.encode(to_encode, SECRET_KEY, algorithm= ALGORITHM)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Response, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from ..import database, schemas, models, utils
+from ..import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=['Authentication'])
 
@@ -13,12 +13,12 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
 
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Invalid Credentials')
+    # create a token
+    # return a token
 
-    
+    access_token = oauth2.create_access_token(data = {"user_id": user.id})
 
-
-
-    return {'token':'valid'}
+    return {'access_token': access_token, "token_type": "bearer"}
 
 
 
