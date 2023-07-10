13b5810c6d949788bbc2133d09653805ecd3e9e1
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-10 14:27:53 +0200

    modified oauth2.py - added a verify_access_token() method.

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,6 @@
 from jose import JWTError, JWTError, jwt
 from datetime import datetime, timedelta
+from. import schemas
 
 # SECRET_KEY
 # Algorithm
@@ -18,6 +19,19 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm= ALGORITHM)
 
     return encoded_jwt
+
+def verify_access_token(token:str, credentials_exception):
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id : str = payload.get("users_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+
 
 
 
