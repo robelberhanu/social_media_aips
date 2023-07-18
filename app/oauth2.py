61f56731738f20cd0fa43a737de5a5f9e28efc0a
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-18 14:10:51 +0200

    removed unwanted code from oauth2.py

--- app/oauth2.py
+++ app/oauth2.py
@@ -12,7 +12,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRATION_MINUTES = 10
+ACCESS_TOKEN_EXPIRATION_MINUTES = 30
 
 # This methid is used to create an access token when a user is logged in.
 def create_access_token(data: dict):
@@ -34,11 +34,8 @@ def verify_access_token(token:str, credentials_exception):
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
-    except JWTError as e:
-        print(e)
+    except JWTError:
         raise credentials_exception
-    except AssertionError as e:
-        print(e)
     
     return token_data
     
