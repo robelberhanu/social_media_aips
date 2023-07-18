a0294b54a592298455834a91aa7ff07906d1274f
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-18 13:20:00 +0200

    modified oauth2.py - corrected exception handling.

--- app/oauth2.py
+++ app/oauth2.py
@@ -12,7 +12,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRATION_MINUTES = 30
+ACCESS_TOKEN_EXPIRATION_MINUTES = 10
 
 # This methid is used to create an access token when a user is logged in.
 def create_access_token(data: dict):
@@ -34,8 +34,11 @@ def verify_access_token(token:str, credentials_exception):
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
-    except JWTError:
+    except JWTError as e:
+        print(e)
         raise credentials_exception
+    except AssertionError as e:
+        print(e)
     
     return token_data
     
